557160f45df391c06feb70d827693f1b832ddb7e
mjkim610 <mjkim610@gmail.com>
2017-08-11 17:56:27 +0900

    Add bot code

---
+++ bookkeep_codehook.py
@@ -0,0 +1,182 @@
+"""
+An implementation of the Lex Code Hook Interface based on AWS sample bot which manages orders for flowers.
+"""
+import time
+import math
+import re
+import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+""" --- Helpers to build responses which match the structure of the necessary dialog actions --- """
+
+
+def get_slots(intent_request):
+    return intent_request['currentIntent']['slots']
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message
+        }
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
+def delegate(session_attributes, slots):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Delegate',
+            'slots': slots
+        }
+    }
+
+
+""" --- Helper Functions --- """
+
+
+def isvalid_user(user):
+    users = ['mjkim', 'mj', 'mjkim610']
+    return user.lower() in users
+
+
+def isvalid_float(s):
+    return s.replace('.', '', 1).isdigit()
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    if message_content is None:
+        return {
+            "isValid": is_valid,
+            "violatedSlot": violated_slot,
+        }
+
+    return {
+        'isValid': is_valid,
+        'violatedSlot': violated_slot,
+        'message': {'contentType': 'PlainText', 'content': message_content}
+    }
+
+
+def validate_request_debt(lender, amount):
+    if lender is not None:
+        if not isvalid_user(lender):
+            return build_validation_result(False,
+                                           'Lender',
+                                           '{} is an invalid user. '
+                                           'Please enter a user in the Slack team.'.format(lender))
+
+    # TODO:
+    # Can't tell if this is not working or Lex is handling typos
+    # Bot is ignoring symbols other than `$`
+    if amount is not None:
+        if amount[0] == "$":
+            amount = amount[1:]
+        if not isvalid_float(amount):
+            return build_validation_result(False,
+                                           'Amount',
+                                           '{} is an invalid amount. '
+                                           'Please enter a valid amount of chips.'.format(amount))
+
+    return build_validation_result(True, None, None)
+
+
+""" --- Functions that control the bot's behavior --- """
+
+
+def record_debt(intent_request):
+    """
+    Performs dialog management and fulfillment for ordering flowers.
+    Beyond fulfillment, the implementation of this intent demonstrates the use of the elicitSlot dialog action
+    in slot validation and re-prompting.
+    """
+
+    lender = get_slots(intent_request)["Lender"]
+    amount = get_slots(intent_request)["Amount"]
+    source = intent_request['invocationSource']
+
+    if source == 'DialogCodeHook':
+        # Perform basic validation on the supplied input slots.
+        # Use the elicitSlot dialog action to re-prompt for the first violation detected.
+        slots = get_slots(intent_request)
+
+        validation_result = validate_request_debt(lender, amount)
+        if not validation_result['isValid']:
+            slots[validation_result['violatedSlot']] = None
+            return elicit_slot(intent_request['sessionAttributes'],
+                               intent_request['currentIntent']['name'],
+                               slots,
+                               validation_result['violatedSlot'],
+                               validation_result['message'])
+
+        # Pass the price of the flowers back through session attributes to be used in various prompts defined
+        # on the bot model.
+        output_session_attributes = intent_request['sessionAttributes'] if intent_request['sessionAttributes'] is not None else {}
+        if lender is not None:
+            output_session_attributes['Somevalue'] = len(lender) * 5  # Elegant pricing model
+
+        return delegate(output_session_attributes, get_slots(intent_request))
+
+    # Order the flowers, and rely on the goodbye message of the bot to define the message to the end user.
+    # In a real bot, this would likely involve a call to a backend service.
+    return close(intent_request['sessionAttributes'],
+                 'Fulfilled',
+                 {'contentType': 'PlainText',
+                  'content': 'Okay, I will record {} chips owed to {} in my books'.format(amount, lender)})
+
+
+""" --- Intents --- """
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'record_debt':
+        return record_debt(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
